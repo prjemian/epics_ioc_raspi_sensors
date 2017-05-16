931fcd5e9f55c9fdc2f0cd9e53d84f8225f4222b
Pete R Jemian <prjemian@gmail.com>
2017-05-15 23:20:45 -0500

    #14 start with a unit test

---
+++ raspiApp/src/test_stats.c
@@ -0,0 +1,14 @@
+/*
+ * test_stats.c
+ *
+ * test stats.c
+ */
+
+#include <stdio.h>
+#include "stats.h"
+
+int main(int argc, char *argv[])
+{
+    printf("TODO: test stats.c\n");
+    return(0);
+}
